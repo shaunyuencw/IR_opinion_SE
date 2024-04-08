ff5d841adf3993d3bfdb648c6b5cdeb48032a504
Xavierleeeugene <xavierleeeugene@gmail.com>
2024-04-09 04:24:50 +0800

    ensemble

--- Classification/ensemble.py
+++ Classification/ensemble.py
@@ -3,21 +3,32 @@ import pickle
 import numpy as np
 from sklearn.svm import SVC
 from torch.nn.functional import softmax
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import BertTokenizer, RobertaTokenizer
+from collections import Counter
 
 #load BERT model
-bert_model = torch.load('/bert_train/model_full.pth')
+bert_model = torch.load('/kaggle/input/bert_model/pytorch/model_full/1/model_full.pth')
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
+#load roBERTa model
+
+roberta_model = torch.load('/kaggle/input/roberta/pytorch/roberta/1/model_full.pth')
+
+tokenizer_r = RobertaTokenizer.from_pretrained('roberta-base')
+
 #load SVM model
-svm_pn_model = pickle.load(('positive_model.sav', 'rb'))
-svm_nn_model = pickle.load(('negative_model.sav', 'rb'))
+with open('/kaggle/input/neutral_model/scikitlearn/neutral/1/neutral_model.sav', 'rb') as f:
+    svm_neutral_model = pickle.load(f)
+with open('/kaggle/input/pos_neg_model/scikitlearn/pos_neg/1/pos_neg_model.sav', 'rb') as f:
+    svm_opinion_model = pickle.load(f)
+#svm_neutral_model = pickle.load(('/kaggle/input/neutral_model/scikitlearn/neutral/1/neutral_model.sav', 'rb'))
+#svm_opinion_model = pickle.load(('/kaggle/input/pos_neg_model/scikitlearn/pos_neg/1/pos_neg_model.sav', 'rb'))
 
 #Sentiment Classification and Confidence Score Function
 
 def sentiment_output(input_text):
-
+    
     #BERT PREDICTION
 
     bert_input = tokenizer(input_text, padding=True, truncation=True, max_length=512, return_tensors="pt")
@@ -33,56 +44,81 @@ def sentiment_output(input_text):
     
     predicted_class = torch.argmax(probs, dim=-1).item()
 
-    # Decode the predicted class index back to the label
-    labels = ['Negative', 'Neutral', 'Positive']
-
     #Get BERT sentiment and confidence
     bert_sentiment = predicted_class
     bert_confidence = probs[0, predicted_class].item()
 
+    #roBERTa PREDICTION
+
+    roberta_input = tokenizer_r(input_text, padding=True, truncation=True, max_length=512, return_tensors="pt")
+
+    if torch.cuda.is_available():
+        roberta_input = {k: v.to("cuda") for k, v in roberta_input.items()}
+        roberta_model.to("cuda")
+
+    # Get predictions
+    with torch.no_grad():
+        roberta_outputs = roberta_model(**roberta_input)
+    roberta_probs = softmax(roberta_outputs.logits, dim=-1)
+    
+    roberta_predicted_class = torch.argmax(roberta_probs, dim=-1).item()
+
+    #Get BERT sentiment and confidence
+    roberta_sentiment = roberta_predicted_class
+    roberta_confidence = roberta_probs[0, roberta_predicted_class].item()
+
     #SVM PREDICTION
 
-    if bert_sentiment == 2:
-        prob_list = svm_pn_model.predict_proba(input_text)[0]
-        svm_confidence = max(prob_list)
-        svm_sentiment = prob_list.index(svm_confidence) + 1 #Shift 1 to get correct sentiment classification index
+    check_neutrality = svm_neutral_model.predict_proba([input_text])[0]
+    
 
-    elif bert_sentiment == 0:
-        prob_list = svm_nn_model.predict_proba(input_text)[0]
-        svm_confidence = max(prob_list)
-        svm_sentiment = prob_list.index(svm_confidence)
+    best_confidence = max(check_neutrality)
+    svm_sentiment = np.argmax(check_neutrality)
+    
+    check_polarity = svm_opinion_model.predict_proba([input_text])[0]
+    polarity_confidence = max(check_polarity)
+    
+    if svm_sentiment == 0:
+        svm_sentiment = 1
+        svm_confidence = best_confidence
     
     else:
-        check_pos = svm_pn_model.predict_proba(input_text)[0]
-        check_neg = svm_nn_model.predict_proba(input_text)[0]
-
-        pos_confidence = max(check_pos)
-        pos_sentiment = prob_list.index(pos_confidence) + 1 #Shift 1 to get correct sentiment classification index
-
-        neg_confidence = max(check_neg)
-        neg_sentiment = prob_list.index(neg_confidence)
-
-        if pos_sentiment == neg_sentiment:
-            svm_sentiment = 1
-        elif pos_sentiment == 2 and neg_sentiment == 0:
-            svm_sentiment = 1
-        elif pos_sentiment == 2 and neg_sentiment == 1:
-            svm_sentiment = 2
-        else:
+        if np.argmax(check_polarity) == 0:
             svm_sentiment = 0
+        else:
+            svm_sentiment = 2
+        svm_confidence = polarity_confidence
+    svm_con_arr = [check_polarity[0],check_neutrality[0],check_polarity[1]]
 
-        svm_confidence = check_pos[0] *0.5 + check_neg[1] * 0.5
-        
 
 
+    #Final sentiment and Final Confidence
+    predictions = [bert_sentiment, roberta_sentiment]
+    count = Counter(predictions)
+    final_sentiment, num_occurrences = count.most_common(1)[0]
 
-
-    #Final sentiment
-    final_sentiment = round(bert_sentiment * 0.6 + svm_sentiment * 0.4)
-
-    #Final confidence
-    final_confidence = bert_confidence * 0.6 + svm_confidence * 0.4
+    # Check if there is a majority
+    if num_occurrences == 1:
+        #print("FLAG 1")
+        #SVM is used as tiebreaker
+        if svm_sentiment in predictions:
+            final_sentiment = svm_sentiment
+            final_confidence = (probs[0, final_sentiment].item() + roberta_probs[0, final_sentiment].item() + svm_con_arr[final_sentiment]) /3
+        #If still tie, roBERTa is used as the final sentiment
+        else:
+            final_sentiment = roberta_sentiment
+            final_confidence = (probs[0, final_sentiment].item() + roberta_probs[0, final_sentiment].item()) /2
+    else:
+        #print("FLAG 2")
+        final_sentiment = final_sentiment
+        final_confidence = (probs[0, final_sentiment].item() + roberta_probs[0, final_sentiment].item()) /2
 
     labels = ['Negative', 'Neutral', 'Positive']  # Decode the predicted class index back to the label
 
     return [labels[final_sentiment], final_confidence]
+
+# print(sentiment_output("Meta announces highest earnings"))
+
+# print(sentiment_output("Meta announces lowest earnings"))
+
+# print(sentiment_output("Meta announces earnings"))
